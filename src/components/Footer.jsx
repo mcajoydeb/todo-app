53250e7ac0303d6e665216afe485b030568bdee1
Joydeb Choudhury <mcajoydeb@gmail.com>
2026-04-01 15:59:42 +0530

    added footer component It is not mentionied in the problem statement but to good UI i have added this

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer style={styles.footer}>
+      <p>© {new Date().getFullYear()} To-Do App | All Rights Reserved</p>
+    </footer>
+  );
+}
+
+
+
+export default Footer;
